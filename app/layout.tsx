9c53ddcd6aa8a582ba77d9a3e06760bf76184d55
Ayush Srivastava <ayushsrivastavawork@gmail.com>
2025-04-07 00:31:09 +0530

    Added Vercel Speed Insights Component

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -17,6 +18,7 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
         <html lang="en" data-oid="mf-e613">
             <body className={inter.className} data-oid="uvtv36w">
                 {children}
+                <SpeedInsights data-oid="ln3fmsf" />
                 <Analytics data-oid="fq3ej_h" />
             </body>
         </html>
